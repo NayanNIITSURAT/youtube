151845bcaf22080431ced85601abd8bef8fc4727
Kirtikumar <avaiyakirtib@gmail.com>
2022-02-24 15:53:02 +0530

    final ui
    ok

---
+++ lib/screens/nav_screen.dart
@@ -0,0 +1,231 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_youtube_ui/data.dart';
+import 'package:flutter_youtube_ui/models/channel_model.dart';
+import 'package:flutter_youtube_ui/models/video_model.dart';
+import 'package:flutter_youtube_ui/screens/home_screen.dart';
+import 'package:flutter_youtube_ui/screens/video_screen.dart';
+import 'package:flutter_youtube_ui/services/api_service.dart';
+import 'package:miniplayer/miniplayer.dart';
+
+final selectedVideoProvider = StateProvider<Video?>((ref) => null);
+
+final miniPlayerControllerProvider =
+    StateProvider.autoDispose<MiniplayerController>(
+  (ref) => MiniplayerController(),
+);
+
+class NavScreen extends StatefulWidget {
+  @override
+  _NavScreenState createState() => _NavScreenState();
+}
+
+class _NavScreenState extends State<NavScreen> {
+  late Channel _channel;
+  bool _isLoading = false;
+  static const double _playerMinHeight = 60.0;
+
+  int _selectedIndex = 0;
+
+  final _screens = [
+    HomeScreen(),
+    const Scaffold(body: Center(child: Text('Explore'))),
+    const Scaffold(body: Center(child: Text('Add'))),
+    const Scaffold(body: Center(child: Text('Subscriptions'))),
+    const Scaffold(body: Center(child: Text('Library'))),
+  ];
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    _initChannel();
+
+  }
+
+  _buildProfileInfo() {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      padding: EdgeInsets.all(20.0),
+      height: 100.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            offset: Offset(0, 1),
+            blurRadius: 6.0,
+          ),
+        ],
+      ),
+      child: Row(
+        children: <Widget>[
+          CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 35.0,
+            backgroundImage: NetworkImage(_channel.profilePictureUrl),
+          ),
+          SizedBox(width: 12.0),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  _channel.title,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  '${_channel.subscriberCount} subscribers',
+                  style: TextStyle(
+                    color: Colors.grey[600],
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+  _buildVideo(Video video) {
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => VideoScreen(id: video.id),
+        ),
+      ),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+        padding: EdgeInsets.all(10.0),
+        height: 140.0,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0, 1),
+              blurRadius: 6.0,
+            ),
+          ],
+        ),
+        child: Row(
+          children: <Widget>[
+            Image(
+              width: 150.0,
+              image: NetworkImage(video.thumbnailUrl),
+            ),
+            SizedBox(width: 10.0),
+            Expanded(
+              child: Text(
+                video.title,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  _loadMoreVideos() async {
+    _isLoading = true;
+    List<Video> moreVideos = await APIService.instance
+        .fetchVideosFromPlaylist(playlistId: _channel.uploadPlaylistId);
+    List<Video> allVideos = _channel.videos..addAll(moreVideos);
+    setState(() {
+      _channel.videos = allVideos;
+    });
+    _isLoading = false;
+  }
+  _initChannel() async {
+    Channel channel = await APIService.instance
+        .fetchChannel(channelId: 'UC6Dy0rQ6zDnQuHQ1EeErGUA');
+    setState(() {
+      _channel = channel;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:
+      _channel != null
+          ? NotificationListener<ScrollNotification>(
+        onNotification: (ScrollNotification scrollDetails) {
+          if (!_isLoading &&
+              _channel.videos.length != int.parse(_channel.videoCount) &&
+              scrollDetails.metrics.pixels ==
+                  scrollDetails.metrics.maxScrollExtent) {
+            _loadMoreVideos();
+          }
+          return false;
+        },
+        child: ListView.builder(
+          itemCount: 1 + _channel.videos.length,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return _buildProfileInfo();
+            }
+            Video video = _channel.videos[index - 1];
+            return _buildVideo(video);
+          },
+        ),
+      )
+          : Center(
+        child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(
+            Theme.of(context).primaryColor, // Red
+          ),
+        ),
+      ),
+
+
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: (i) => setState(() => _selectedIndex = i),
+        selectedFontSize: 10.0,
+        unselectedFontSize: 10.0,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore_outlined),
+            activeIcon: Icon(Icons.explore),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_outline),
+            activeIcon: Icon(Icons.add_circle),
+            label: 'Add',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.subscriptions_outlined),
+            activeIcon: Icon(Icons.subscriptions),
+            label: 'Subscriptions',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.video_library_outlined),
+            activeIcon: Icon(Icons.video_library),
+            label: 'Library',
+          ),
+        ],
+      ),
+    );
+  }
+}
